ede80aa0ccfc1dc1a798718e7dcf59fcb63e4c7e
arjunkdot <78548960+arjunkdot@users.noreply.github.com>
2024-03-10 20:49:23 +0530

    WIP: add useThunk hook

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,18 +1,28 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchUsers } from "../store";
+import React, { useEffect} from "react";
+import { useSelector } from "react-redux";
+import { useThunk } from "../hooks/useThunk";
+import { fetchUsers, addUser } from "../store";
 import Skeleton from "./Skeleton";
+import Button from "./Button";
+
 
 export default function UsersList() {
-  const dispatch = useDispatch();
-  const { isLoading, data, error } = useSelector((state) => state.users);
+  const [doFetchUsers, isLoadingUsers, loadingUsersError] =
+    useThunk(fetchUsers);
+  const [doCreateUsers, isCreatingUser, creatingUserError] = useThunk(addUser);
+
+  const { data } = useSelector((state) => state.users);
 
   useEffect(() => {
-    dispatch(fetchUsers());
-  }, [dispatch]);
+    doFetchUsers();
+  }, [doFetchUsers]);
 
-  if (isLoading) return <Skeleton times={6} className="h-10 w-full" />;
-  if (error) return <div>Error fetching data</div>;
+  const handleUserAdd = () => {
+    doCreateUsers();
+  };
+
+  if (isLoadingUsers) return <Skeleton times={6} className="h-10 w-full" />;
+  if (loadingUsersError) return <div>Error fetching data</div>;
 
   const renderedUsers = data.map((user) => (
     <div key={user.id} className="mb-2 border rounded">
@@ -21,5 +31,18 @@ export default function UsersList() {
       </div>
     </div>
   ));
-  return <div>{renderedUsers}</div>;
+  return (
+    <div>
+      <div className="flex flex-row justify-between m-3">
+        <h1 className="m2 text-xl">Users</h1>
+        {isCreatingUser ? (
+          "Creating user..."
+        ) : (
+          <Button onClick={handleUserAdd}>+ Add User</Button>
+        )}
+        {creatingUserError ? creatingUserError : null}
+      </div>
+      {renderedUsers}
+    </div>
+  );
 }
